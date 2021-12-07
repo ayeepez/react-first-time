da9bf68b8ba282b60b6974f310e2d85c50ea5045
ayeepez <ayeepez@gmail.com>
2021-12-06 21:35:39 -0500

    class 1 layout HTML

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+
+import "./App.css";
 
 function App() {
+  //  javascriptt
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hola soy Amy y no se GitHub 
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <div className="header__title"> Amairany Yepez </div>
+        <div className="header__menu">Menu</div>
       </header>
+
+      <main className="main-container">
+        <h1 className="main-container__title">texto grande</h1>
+        <div className="main-container__box">Container</div>
+      </main>
+
+      <footer className="footer-container">
+        <div className="social-media"> Social media </div>
+        <div className="contact-me">Contact me</div>
+      </footer>
+   
     </div>
   );
 }
